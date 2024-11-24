491467860ffa6c3527dfca438660096813aa9ce4
HDNghia <huynhdangnghia68@gmail.com>
2024-11-24 12:46:04 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Products from "./components/Products/Products";
 import ProductsPage from "./components/PageProduct/ProductsPage";
 import Footer from "./components/Footer/Footer";
